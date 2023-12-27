c4f572e3ee628aa4cee00d508f2cffbc05a58f16
pcarmo <pedro96.carmo@hotmail.com>
2023-12-27 18:35:24 +0000

    Add install script to setup tmux

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function setup_tmux(){
+    filename='.tmux.conf'
+    target="$HOME/$filename" 
+
+    if test -e "$target"; then 
+        if [[ "$1" == "force" ]]; then
+            echo "Forcing update."
+            rm -r $target
+        else
+            echo "Failed to update $target, it already exists. If you really wish to update, force it."
+            return
+        fi
+    fi
+
+    source="`pwd`/$filename"
+
+    echo "creating file $target"
+    echo "and linking to $source"
+    ln -s $source $target
+}
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Detected Linux..."
+    setup_tmux
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Detected MacOS..."
+    setup_tmux "force"
+
+else
+    echo "Using unsupported OS"
+fi
